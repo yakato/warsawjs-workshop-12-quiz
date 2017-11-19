9cda1fca4fb64a7198eb23bca51d54df3de0e11b
yakato <frey.kasia@gmail.com>
2017-11-19 11:44:02 +0100

    Basic version of quiz app

---
+++ src/Question.vue
@@ -0,0 +1,13 @@
+{/*<template>
+  <section id="question">
+    {{ quiz-question }}
+  </section>
+</template>
+
+<script>
+  export default {
+    name: 'question',
+    props: ['quiz-question']
+  }
+</script>
+*/}
---
+++ src/Quiz.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="container">
+    <div>
+    {{ currentQuestion.text }}
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+
+    props: ['allQuestions'],
+
+    data: function() {
+      return {
+        currentQuestion: this.allQuestions.questions[0],
+      }
+    },
+  }
+</script>
