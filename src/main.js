2d57b86c6294ad66f4bfc622fbb8b75d1616bab4
yakato <frey.kasia@gmail.com>
2017-11-19 15:08:44 +0100

    Move import json

--- src/main.js
+++ src/main.js
@@ -4,10 +4,13 @@ import App from './App.vue'
 import router from './router'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
-
+import quizzes from './quizzes_json.json'
 
 new Vue({
   el: '#app',
   router,
-  render: h => h(App)
+  render: h => h(App),
+  data: {
+    quizzes: quizzes.quizzes,
+  }
 })
